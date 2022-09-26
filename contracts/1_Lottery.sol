7ffb2943f02cb1841dee21ad46ecd6b37c7dcf38
Ahmet Nusret ASMA <ahmet2nusret7@gmail.com>
2022-09-26 10:48:19 +0300

    The Lottery (Sweepstakes) Contract

---
+++ contracts/1_Lottery.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    
+    address public manager;
+    address[] public players;
+
+    constructor() public {
+        manager = msg.sender;
+    }
+
+    modifier checkManager(){
+        require(msg.sender == manager);
+        _;
+    }
+
+    function enter() public payable{
+        require( msg.value > .00001 ether );
+        players.push(msg.sender);
+    }
+
+    function getPlayers() public view returns(address[]){
+        return players;
+    }
+
+    function randomHelper() private view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+    }
+
+    function pickTheWinner() public payable checkManager{
+        uint index = randomHelper() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);   // yeni bir dinamik players dizisi oluştur ve bunu 0. elemandan başlat edik bu şekilde oyunumuz sıfırlanmış oldu.
+    }
+
+}
